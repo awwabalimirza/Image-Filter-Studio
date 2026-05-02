cc6ff5da74ce5225251c78e01d861968566393b5
awwabalimirza <awwabalimirza56@gmail.com>
2026-05-02 19:16:50 +0500

    Refactor FilterSession class to include additional member variables and methods for enhanced functionality

--- FilterSession.h
+++ FilterSession.h
@@ -2,12 +2,40 @@
 #define FILTERSESSION_H
 #include<iostream>
 #include<vector>
+#include<fstream>
+#include<sstream>
+#include<string>
+#include<ctime>
+#include "image.h"
+#include "filter.h"
 using namespace std;
 
 class FilterSession{
 private:
-    vector<string> pipeline;
+    Image* image;
+    vector<Filter*> pipeline;
+    string cnic;
+    string timestamp;
+    string outputFile;
+
+    string generateTimestamp();
+    string buildFilterString();
+
+public:
+    FilterSession(Image* img, string cnic);
+    ~FilterSession();
+
+    FilterSession& addFilter(Filter* f);
+    void clearPipeline();
+    void displayPipeline();
+    int getPipelineSize();
+
+    void applyPipeline();
+    bool saveResult(string fmt);
+    void recordSession();
+
+    string getOutputFile();
+    string getTimestamp();
 };
 
 #endif
-
